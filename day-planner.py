f529d2e8b319755152ed7231f79c951a783fc032
srebrny <p.srebniak@gmail.com>
2016-01-02 00:26:21 +0100

    Update day plan.

--- day-planner.py
+++ day-planner.py
@@ -6,6 +6,8 @@ __copyright__ = u'Copyright (c) 2010 Tomasz Świderski'
 import Image, ImageDraw, ImageFont
 import sys
 
+from decorators import BackgroundDecorator, JustifyTextDecorator
+
 SEN = []
 PRACA_I_OBIAD = []
 FIRMA = []
@@ -17,66 +19,93 @@ SOO = []  # sprzątanie / obiad / odpoczynek
 
 day1 = {
     "header": "Plan dnia - Normalny",
-    "hours": {
-        5: "PRZYGOTOWANIE",
-        6: "FIRMA",
-        7: "FIRMA",
-        8: "PRACA i Obiad",
-        9: "PRACA i Obiad",
-        10: "PRACA i Obiad",
-        11: "PRACA i Obiad",
-        12: "PRACA i Obiad",
-        13: "PRACA i Obiad",
-        14: "PRACA i Obiad",
-        15: "PRACA i Obiad",
-        16: "PRACA i Obiad",
-        17: "PRACA i Obiad",
-        18: "SPRZATANIE/OBIAD/ODPOCZYNEK",
-        19: "DZHK",
-        20: "FIRMA",
-        21: "FIRMA",
-        22: "PRZYGOTOWANIE",
-        23: "SEN",
-        00: "SEN",
-        01: "SEN",
-        02: "SEN",
-        03: "SEN",
-        04: "SEN",
-    }
+    "hours": [
+        {"hour": "05", "text": "PRZYGOTOWANIE"},
+        {"hour": "06", "text": "FIRMA"},
+        {"hour": "07", "text": "FIRMA"},
+        {"hour": "08", "text": "PRACA i Obiad"},
+        {"hour": "09", "text": "PRACA i Obiad"},
+        {"hour": "10", "text": "PRACA i Obiad"},
+        {"hour": "11", "text": "PRACA i Obiad"},
+        {"hour": "12", "text": "PRACA i Obiad"},
+        {"hour": "13", "text": "PRACA i Obiad"},
+        {"hour": "14", "text": "PRACA i Obiad"},
+        {"hour": "15", "text": "PRACA i Obiad"},
+        {"hour": "16", "text": "PRACA i Obiad"},
+        {"hour": "17", "text": "PRACA i Obiad"},
+        {"hour": "18", "text": "SPRZATANIE/OBIAD/ODPOCZYNEK"},
+        {"hour": "19", "text": "DZHK"},
+        {"hour": "20", "text": "FIRMA"},
+        {"hour": "21", "text": "FIRMA"},
+        {"hour": "22", "text": "PRZYGOTOWANIE"},
+        {"hour": "23", "text": "SEN"},
+        {"hour": "00", "text": "SEN"},
+        {"hour": "01", "text": "SEN"},
+        {"hour": "02", "text": "SEN"},
+        {"hour": "03", "text": "SEN"},
+        {"hour": "04", "text": "SEN"},
+    ]
 }
 
 day2 = {
     "header": "Plan dnia - Soboty",
-    "hours": {
-        5: "SEN",
-        6: "SEN",
-        7: "SEN",
-        8: "SEN",
-        9: "PRZYGOTOWANIE",
-        10: "SPRZATANIE/OBIAD/ODPOCZYNEK",
-        11: "PRACA i Obiad",
-        12: "PRACA i Obiad",
-        13: "PRACA i Obiad",
-        14: "PRACA i Obiad",
-        15: "PRACA i Obiad",
-        16: "PRACA i Obiad",
-        17: "SPRZATANIE/OBIAD/ODPOCZYNEK",
-        18: "DZHK",
-        19: "DZHK",
-        20: "DZHK",
-        21: "DZHK",
-        22: "DZHK",
-        23: "DZHK",
-        00: "DZHK",
-        01: "PRZYGOTOWANIE",
-        02: "SEN",
-        03: "SEN",
-        04: "SEN",
-    }
+    "hours": [
+        {"hour": "05", "text": "SEN"},
+        {"hour": "06", "text": "SEN"},
+        {"hour": "07", "text": "SEN"},
+        {"hour": "08", "text": "SEN"},
+        {"hour": "09", "text": "PRZYGOTOWANIE"},
+        {"hour": "10", "text": "SPRZATANIE/OBIAD/ODPOCZYNEK"},
+        {"hour": "11", "text": "FIRMA"},
+        {"hour": "12", "text": "FIRMA"},
+        {"hour": "13", "text": "FIRMA"},
+        {"hour": "14", "text": "FIRMA"},
+        {"hour": "15", "text": "FIRMA"},
+        {"hour": "16", "text": "FIRMA"},
+        {"hour": "17", "text": "SPRZATANIE/OBIAD/ODPOCZYNEK"},
+        {"hour": "18", "text": "DZHK"},
+        {"hour": "19", "text": "DZHK"},
+        {"hour": "20", "text": "DZHK"},
+        {"hour": "21", "text": "DZHK"},
+        {"hour": "22", "text": "DZHK"},
+        {"hour": "23", "text": "DZHK"},
+        {"hour": "00", "text": "DZHK"},
+        {"hour": "01", "text": "PRZYGOTOWANIE"},
+        {"hour": "02", "text": "SEN"},
+        {"hour": "03", "text": "SEN"},
+        {"hour": "04", "text": "SEN"},
+    ]
 }
 
-day3 = day2.copy()
-day3["header"] = "Plan dnia - Święta"
+day3 = {
+    "header": "Plan dnia - Święta",
+    "hours": [
+        {"hour": "05", "text": "SEN"},
+        {"hour": "06", "text": "SEN"},
+        {"hour": "07", "text": "SEN"},
+        {"hour": "08", "text": "SEN"},
+        {"hour": "09", "text": "PRZYGOTOWANIE"},
+        {"hour": "10", "text": "SPRZATANIE/OBIAD/ODPOCZYNEK"},
+        {"hour": "11", "text": "FIRMA - ROZWÓJ"},
+        {"hour": "12", "text": "FIRMA - ROZWÓJ"},
+        {"hour": "13", "text": "FIRMA - ROZWÓJ"},
+        {"hour": "14", "text": "FIRMA - ROZWÓJ"},
+        {"hour": "15", "text": "FIRMA - ROZWÓJ"},
+        {"hour": "16", "text": "FIRMA - ROZWÓJ"},
+        {"hour": "17", "text": "SPRZATANIE/OBIAD/ODPOCZYNEK"},
+        {"hour": "18", "text": "DZHK"},
+        {"hour": "19", "text": "DZHK"},
+        {"hour": "20", "text": "DZHK"},
+        {"hour": "21", "text": "DZHK"},
+        {"hour": "22", "text": "DZHK"},
+        {"hour": "23", "text": "DZHK"},
+        {"hour": "00", "text": "DZHK"},
+        {"hour": "01", "text": "PRZYGOTOWANIE"},
+        {"hour": "02", "text": "SEN"},
+        {"hour": "03", "text": "SEN"},
+        {"hour": "04", "text": "SEN"},
+    ]
+}
 
 
 class DayPlan(object):
@@ -89,62 +118,68 @@ class DayPlan(object):
         self.draw = ImageDraw.Draw(self.im)
         # margins Left, upper, right, bottom
         self.marginsCols = {"size": 320}
-        self.marginsHeader = {"left": 55, "top": 52, "right": 25, "bottom": 25}
+        self.marginsHeader = {"left": 15, "top": 52, "right": 25, "bottom": 25}
         self.marginsRows = {"left": 25, "top": 52, "right": 25, "bottom": 25}
         self.padding_cols = 18
 
-        self.HeaderFont = ImageFont.truetype("./NotoSans-BoldItalic.ttf", 20)
-        self.TextFont = ImageFont.truetype("./NotoSans-Regular.ttf", 15)
+        #
+        # Font Settings
+        #
+        self.HeaderFont = ImageFont.truetype("./fonts/NotoSans-BoldItalic.ttf", 20)
+        self.TextFont = ImageFont.truetype("./fonts/NotoSans-Regular.ttf", 15)
+
+        self.TextFontFill_hour = (255, 255, 255)
+        self.TextFontFill_text = (255, 255, 255)
 
     def setData(self, *kargs):
-        cols = 0
+        self.col_i = 0
         for day in kargs:
             HeaderPos = (
-                self.marginsHeader["left"] + (self.marginsCols["size"] * cols + 20 + self.marginsHeader["right"]), 23)
-            print("Marginesy dla kolumny %i " % cols, HeaderPos)
+                self.marginsHeader["left"] + (self.marginsCols["size"] * self.col_i + 20 + self.marginsHeader["right"]),
+                23)
+            print("Marginesy dla kolumny %i " % self.col_i, HeaderPos)
 
             self.draw.text(HeaderPos,
                            day["header"].decode("utf-8"), font=self.HeaderFont,
                            fill=(240, 240, 0))
-            row_i = 0
-            for hour, text in day["hours"].iteritems():
+            self.row_i = 0
+            for row in day["hours"]:
                 RowNumPos = (
-                    self.marginsRows["left"] + (self.marginsCols["size"] * cols),
-                    self.marginsRows["top"] + (row_i * 22)
-                )
-                RoWTexPos = (
-                    self.marginsRows["left"] + 50 + (self.marginsCols["size"] * cols),
-                    self.marginsRows["top"] + (row_i * 22)
+                    self.marginsRows["left"] + (self.marginsCols["size"] * self.col_i),
+                    self.marginsRows["top"] + (self.row_i * 22)
                 )
 
-                print("Marginesy dla kolumny %i Numer: %s, Text: %s  " % (cols, RowNumPos, RoWTexPos))
-                self.draw.text(RowNumPos,
-                               str("%s%s" % ("  ", hour)).decode("utf-8"),
-                               font=self.TextFont,
-                               fill=(240, 240, 0))
-                self.draw.text(RoWTexPos, text.decode("utf-8"), font=self.TextFont,
-                               fill=(240, 240, 0))
-                row_i += 1
+                RowTexPos = (
+                    self.marginsRows["left"] + 30 + (self.marginsCols["size"] * self.col_i),
+                    self.marginsRows["top"] + (self.row_i * 22)
+                )
 
-            cols += 1
+                self.draw_row(row["hour"], row["text"], background=(0, 0, 128),
+                              RowNumPos=RowNumPos, RowTexPos=RowTexPos, justify=True)
+                self.row_i += 1
+                # break
 
-    # # Każda linia ma mieć odstęp od góry - 5 px + font size + 2px, Boki 10px + TextFont.getSize(t)+ 10px
-    #     #
-    #     # draw.rectangle([margins["left"], 23 + (i * 22), 50, 50], fill=(255, 0, 0), outline=(0, 255, 0))
-    #     # write hour cell
-    #     draw.text((margins["left"], 23 + (i * 22)), str("%s%s" % ("  ", h)).decode("utf-8"), font=TextFont,
-    #               fill=(240, 240, 0))
-    #
-    #     # Write text cell
-    #     draw.text((margins["left"] + 50, 23 + (i * 22)), t.decode("utf-8"), font=TextFont, fill=(240, 240, 0))
-    #
-    #     print(h, " => ", t)
-    #     i += 1
-    #
-    # del draw
+            self.col_i += 1
 
-    def draw_row(hour, text):
-        pass
+    @BackgroundDecorator
+    @JustifyTextDecorator
+    def draw_row(self, hour, text, background=None, justify=None, RowNumPos=(0, 0), RowTexPos=(0, 0)):
+
+        print("Marginesy dla kolumny %i Numer: %s, Text: %s  " % (self.col_i, RowNumPos, RowTexPos))
+
+        self.draw.text(
+            RowNumPos,
+            hour.decode("utf-8"),
+            font=self.TextFont,
+            fill=self.TextFontFill_hour
+        )
+
+        self.draw.text(
+            RowTexPos,
+            text.decode("utf-8"),
+            font=self.TextFont,
+            fill=self.TextFontFill_text
+        )
 
     def save(self):
         self.im.save(self.filename)
--- decorators.py
+++ decorators.py
@@ -1,1 +1,64 @@
 __author__ = 'srebrny'
+
+
+def BackgroundDecorator(f):
+    def wrapper(*argv, **kwargs):
+        self = argv[0]
+        background = kwargs.get("background", None)
+
+        if background:
+            # Draw a background for our hour
+            bHour_coords = [
+                (
+                    self.marginsRows["left"] + (self.marginsCols["size"] * self.col_i) - 5,
+                    self.marginsRows["top"] + (self.row_i * 22)
+                ),
+                (
+                    self.marginsRows["left"] + (self.marginsCols["size"] * self.col_i) + 22,
+                    self.marginsRows["top"] + (self.row_i * 22) + 24
+                )
+            ]
+
+            bText_coords = [
+                (
+                    self.marginsRows["left"] + (self.marginsCols["size"] * self.col_i) + 22,
+                    self.marginsRows["top"] + (self.row_i * 22)
+                ),
+                (
+                    self.marginsRows["left"] + (self.marginsCols["size"] * self.col_i) + self.marginsCols["size"] - 20,
+                    self.marginsRows["top"] + (self.row_i * 22) + 22
+                )
+            ]
+
+            # Border for hour coll
+            self.draw.rectangle(bHour_coords, fill=background, outline=(128, 128, 128))
+
+            # Border for text coll
+            self.draw.rectangle(bText_coords, fill=background, outline=(128, 128, 128))
+
+        return f(*argv, **kwargs)
+
+    return wrapper
+
+
+def JustifyTextDecorator(f):
+    def wrapper(*argv, **kwargs):
+        self = argv[0]
+        hour = argv[1]
+        text = argv[2]
+        justify = kwargs.get("justify", False)
+
+        # if we want to justify our text...
+        if justify:
+            # We grab RowTexPos
+            RowTexPos = kwargs.get("RowTexPos", (0, 0))
+            # calculate current text length and width ...
+            calculate_text_size = self.draw.textsize(text, self.TextFont)
+            padding_size = (self.marginsCols["size"] - calculate_text_size[0] - 55) / 2
+
+            # and update the kwargs
+            kwargs["RowTexPos"] = (RowTexPos[0] + padding_size , RowTexPos[1])
+
+        return f(*argv, **kwargs)
+
+    return wrapper
